41d7f1b6cc17965e39ef7c2e0e0dc9359b52295b
danilasar <danila.sar@yandex.ru>
2024-12-24 01:29:08 +0400

    фича: новые виды систем

---
+++ src/Engine/Systems/ILoadSystem.cpp
@@ -0,0 +1,13 @@
+//
+// Created by danilasar on 24.12.2024.
+//
+
+#include "ILoadSystem.h"
+
+namespace Engine {
+    namespace Systems {
+        void ILoadSystem::Update() {
+            throw Engine::Exceptions::NotImplementedException();
+        }
+    } // Systems
+} // Game
---
+++ src/Engine/Systems/IUnloadSystem.cpp
@@ -0,0 +1,13 @@
+//
+// Created by danilasar on 24.12.2024.
+//
+
+#include "IUnloadSystem.h"
+
+namespace Engine {
+    namespace Systems {
+        void IUnloadSystem::Update() {
+            throw Engine::Exceptions::NotImplementedException();
+        }
+    } // Systems
+} // Game
